63a1b461782c35b435897ef43fd0bc51c2e1e8cf
Chris van Run <chris.vanrun@radboudumc.nl>
2025-10-20 10:47:09 +0200

    Removal of unreachable actions

--- dicom_deid/build_candidate_procedure.py
+++ dicom_deid/build_candidate_procedure.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 
+from dicom_deid.procedure_cleanup import remove_unreachable_actions
 from dicom_deid.procedure_generation import DICOMStandard, Procedure
 from dicom_deid.render import generate_human_readable_format
 
@@ -41,8 +42,26 @@ def main():
     with open(args.manual, "r") as f:
         manual = Procedure.from_json(f.read())
 
+    # Validate manual: it should not have additional actions base
+    # does not prescribe
+
+    for sop_id in manual.sop_ids:
+        manual_actions = manual.get_sop_actions(sop_id=sop_id)
+        base_actions = base.get_sop_actions(sop_id=sop_id)
+        extra_tags = set(manual_actions.keys()) - set(base_actions.keys())
+
+        if extra_tags:
+            raise ValueError(
+                f"Manual override has unexpected tags for {sop_id=}: {extra_tags}"
+            )
+
     candidate = base + manual
 
+    dicom_standard = DICOMStandard.from_path(args.dicom_standard)
+
+    remove_unreachable_actions(procedure=candidate, dicom_standard=dicom_standard)
+
+    # Loop unreachables action removal back to manual
     candidate_json = candidate.to_json(indent=4, sort_keys=True)
 
     with open(args.output, "w") as f:
@@ -50,7 +69,7 @@ def main():
 
     generate_human_readable_format(
         output=args.output.parent / "manual-human",
-        dicom_standard=DICOMStandard.from_path(args.dicom_standard),
+        dicom_standard=dicom_standard,
         procedure=manual,
     )
 
